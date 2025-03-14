4a3d14986abdd3d90fbe1a0af8e4e270ed29329f
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-03-14 09:38:54 -0300

    refactor: mudança no local das pastas

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Loja-de-Moveis/',
+  base: '/loja-de-moveis/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
