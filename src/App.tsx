f26490ec95b27c28fac489f8020ef74ffec3ac75
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-03-14 10:15:05 -0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route  } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header';
 import { HomePage } from './pages/HomePage';
 import { ShopPage } from './pages/ShopPage';
@@ -8,32 +8,22 @@ import { CartPage } from './pages/CartPage';
 import { CheckoutPage } from './pages/CheckoutPage';
 import { AboutPage } from './pages/AboutPage';
 import ProductEdition from './pages/ProductEdition';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 function App() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (window.location.hash === '' || window.location.hash === '#') {
-      navigate('/');
-    }
-  }, []);
-
   return (
     <Router>
       <div className="min-h-screen bg-gray-50">
         <Header />
         <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-          <Route>
+          <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/shop" element={<ShopPage />} />
             <Route path="/product/:id" element={<ProductPage />} />
             <Route path="/edition" element={<ProductEdition />} />
             <Route path="/cart" element={<CartPage />} />
             <Route path="/checkout" element={<CheckoutPage />} />
-            <Route path="/edição" element={<ProductEdition />} />
-          </Route>
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
         </main>
       </div>
     </Router>
