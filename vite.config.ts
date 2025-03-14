7e747a58738218c1028b39c014cb153c60da9f8f
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-03-14 09:25:38 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Loja de Móveis/',
+  base: '/Loja-de-Moveis/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
