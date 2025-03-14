d858c472d6cbca7bbd86b2ea2ada1f1b8ff618ce
Kaiky Pires <kaiky.pires@yahoo.com.br>
2025-03-14 10:01:37 -0300

    refactor: mudança no local das pastas

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Header } from './components/Header';
 import { HomePage } from './pages/HomePage';
 import { ShopPage } from './pages/ShopPage';
@@ -8,8 +8,18 @@ import { CartPage } from './pages/CartPage';
 import { CheckoutPage } from './pages/CheckoutPage';
 import { AboutPage } from './pages/AboutPage';
 import ProductEdition from './pages/ProductEdition';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (window.location.hash === '' || window.location.hash === '#') {
+      navigate('/');
+    }
+  }, []);
+
   return (
     <Router>
       <div className="min-h-screen bg-gray-50">
